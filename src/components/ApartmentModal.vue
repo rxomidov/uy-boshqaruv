5fcadfdb7a06c03e875524716face15359b03817
rixsitillaxomidov <rixsitilloxomidov@gmail.com>
2026-09-18 14:22:09 +0500

    dsds

--- src/components/ApartmentModal.vue
+++ src/components/ApartmentModal.vue
@@ -128,8 +128,7 @@ function clearData() {
       <div class="modal-header">
         <div>
           <h3>Хонадон маълумотлари</h3>
-          <p>{{ cell.block }} блок • {{ cell.floor }}-қават • {{ cell.number }}-хонадон</p>
-          <p>{{ getSquareFor(cell.block, cell.floor, cell.number) }} (m<sup>2</sup>)</p>
+          <p>{{ cell.block }} блок • {{ cell.floor }}-қават • {{ getSquareFor(cell.block, cell.floor, cell.number) }} (m<sup>2</sup>)</p>
         </div>
         <button class="modal-close" type="button" aria-label="Yopish" @click="$emit('close')">✕</button>
       </div>
